71fc6d96d774ce3353aa2c9f211433dd96566468
WengXinqi <374845873@qq.com>
2022-10-29 23:47:21 +0800

    test

---
+++ fileDemo/fileDemo.cpp
@@ -0,0 +1,24 @@
+﻿// fileDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string> 
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+    fstream test;
+    test.open("text.txt", ios::out);
+    test << "张三" << endl << "18" << endl << "男" << endl;
+    test.close();
+
+    test.open("text.txt", ios::in);
+    string str;
+    while (getline(test,str))
+    {
+        cout << str<<endl;
+    }
+}
+
